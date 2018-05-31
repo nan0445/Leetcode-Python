f96ee9429c6de60597297e379f2b18dd030edc24
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-05-31 11:20:30 -0400

    Create Container With Most Water.js

---
+++ Container With Most Water.js
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l=0
+        r=len(height)-1
+        res = 0
+        while l<r:
+            res = max(res,(r-l)*min(height[r],height[l]))
+            if height[r]<height[l]:
+                r-=1
+            else:
+                l+=1
+        return res
