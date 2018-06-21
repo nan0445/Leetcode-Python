888dac07537c09956489581fed68664fac763192
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-06-21 17:22:01 -0400

    Create Implement strStr().py

---
+++ Implement strStr().py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(needle)==0: return 0
+        return haystack.find(needle)
