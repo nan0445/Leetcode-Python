feb09ba5cc34da5fdd4b03f6a06f26d8a270e24a
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-07-04 13:45:40 -0400

    Create Climbing Stairs.py

---
+++ Climbing Stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 1
+        for _ in range(n-1):
+            a, b = a+b, a
+        return a
