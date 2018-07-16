4341cc470e84d30d103e92a64dca06ebe7926b89
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-07-15 21:22:57 -0400

    Create Single Number.py

---
+++ Single Number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        flag = False
+        if len(nums)==1: return nums[0]
+        i = 1
+        while i<len(nums):
+            if nums[i]!=nums[i-1]:
+                if not flag: return nums[i-1]
+                else: 
+                    flag = False
+                    i += 1
+            else:
+                flag = True
+                i += 1
+        return nums[-1]
+ #################################
+ ####Below is a very easy solution#####
+ 
+ class Solution:
+    def singleNumber(self, nums: list) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
