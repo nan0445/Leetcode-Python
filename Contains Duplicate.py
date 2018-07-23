8f4bb150b20264902365a049ccd544344fb702dc
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-07-23 12:50:17 -0400

    Create Contains Duplicate.py

---
+++ Contains Duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else: return True
+        return False
